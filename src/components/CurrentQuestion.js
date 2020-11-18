c6d5b497510f6b31fc942db27b511dbc8a6b22c9
Olof Lindahl <olindahl@gmail.com>
2020-11-18 10:24:09 +0100

    basic styling

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {quiz} from "reducers/quiz"
+import { quiz } from "reducers/quiz";
+import styled from 'styled-components';
 
 
 export const CurrentQuestion = () => {
@@ -21,6 +22,7 @@ export const CurrentQuestion = () => {
     (state) => state.quiz.answers[state.quiz.currentQuestionIndex]
   );
   console.log(answer);
+  const showSummary = false;
 
   //"activate" dispatch hook
   const dispatch = useDispatch()
@@ -37,16 +39,57 @@ export const CurrentQuestion = () => {
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
+    
+  const Button = styled.button`
+  box-shadow: 0px 1px 0px 0px #97c4fe;
+	background: linear-gradient(to bottom, #3d94f6 5%, #1e62d0 100%);
+	background-color: #3d94f6;
+	border-radius: 6px;
+	border: 1px solid #337fed;
+	display: inline-block;
+	cursor: pointer;
+	color: #ffffff;
+	font-family: 'Space Mono', monospace;
+	font-size: 27px;
+	font-weight: bold;
+	padding:25px 76px;
+	text-decoration: none;
+  text-shadow: 0px 1px 0px #3d768a;
+  margin: 20px;
+  
+  &:hover {
+   background: linear-gradient(to bottom, #1e62d0 5%, #3d94f6 100%);
+	 background-color: #1e62d0; 
+  }
+    
+  &:active {
+   position:relative;
+	 top:1px; 
+  }`  
+    
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 100px
+  `
+
+
 
   return (
-    <div>
+    <Container>
       <h1>Question: {question.questionText}</h1>
+      {!answer && // display all options if none has been answered
       <div>
         {question.options.map((answerOption, index) => {
-          return <button type="button" key={index} onClick={() => handleAnswerButton(index)}>{answerOption}</button>;
+          return <Button type="button" key={index} onClick={() => handleAnswerButton(index)}>{answerOption}</Button>;
         })}
-      </div>
+      </div>}
       {answer && (answer.answerIndex === answer.question.correctAnswerIndex ? <p>The answer is correct</p> : <p>Fail!</p>)}
-    </div>
-  );
-};
+      {answer &&  <Button type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>Next question</Button>}
+      {/* {answer &&  <Button type="button" onClick={() => showSummary = True)}>Show your result</Button>} */}
+    </Container>
+  )   
+}
+      
---
+++ src/components/Summary.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "reducers/quiz";
+import styled from 'styled-components';
+
+export const Summary = () => {
+
+}
